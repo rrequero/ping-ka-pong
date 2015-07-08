176c39e5618374ae20acdfbfa974e38bf493b5b5
Raul Requero <raul.requero@native.ad>
2015-07-08 17:51:00 +0200

    add apidoc.json

--- src/routes/UserRoute.js
+++ src/routes/UserRoute.js
@@ -9,87 +9,88 @@ var UserService = appRefs.getService('UserService');
 
 module.exports = function() {
 
-    var API = (function() {
-        var list = function * () {
-            this.body = yield UserService.find({});
-        };
+	var API = (function() {
+		var list = function * () {
+			this.body = yield UserService.find({});
+		};
 
-        var count = function * () {
-            this.body = yield UserService.count({});
-        };
+		var count = function * () {
+			this.body = yield UserService.count({});
+		};
 
-        var create = function * () {
-            try {
-                logger.debug('Creating user with data ', this.request.body);
-                this.body = yield UserService.create(this.request.body);
-            } catch (e) {
-                logger.error(e);
-                this.throw(500, 'Error to insert user');
-            }
-        };
+		var create = function * () {
+			try {
+				logger.debug('Creating user with data ', this.request.body);
+				this.body = yield UserService.create(this.request.body);
+			} catch (e) {
+				logger.error(e);
+				this.throw(500, 'Error to insert user');
+			}
+		};
 
-        var findOne = function * () {
-            this.body = yield UserService.findOne({
-                slug: this.params.slug
-            });
-        };
+		var findOne = function * () {
+			this.body = yield UserService.findOne({
+				slug: this.params.slug
+			});
+		};
 
-        return {
-            list: list,
-            create: create,
-            count: count,
-            findOne: findOne
-        };
-    }());
+		return {
+			list: list,
+			create: create,
+			count: count,
+			findOne: findOne
+		};
+	}());
 
-    /**
-     * @api {get} /user Request all users
-     * @apiName List
-     * @apiGroup User
-     * @apiVersion 1.0.0
-     *
-     * @apiParam {String} slug user unique.
-     *
-     * @apiSuccess {Object[]} users List of users
-     */
-    UserRouter.get('/user', API.list);
-
-    /**
-     * @api {post} /user Request create user
-     * @apiName Create
-     * @apiGroup User
+	/**
+	 * @api {get} /user Request all users
+	 * @apiName List
+	 * @apiGroup User
 	 * @apiVersion 1.0.0
-     *
-     * @apiParam {String} name Name of the user
-     * @apiParam {String} email Email of the user
-     *
-     */
-    UserRouter.post('/user', API.create);
+	 *
+	 * @apiParam {String} slug user unique.
+	 *
+	 * @apiSuccess {Object[]} users List of users
+	 */
+	UserRouter.get('/user', API.list);
 
-    /**
-     * @api {get} /user/count Request count users
-     * @apiName Count
-     * @apiGroup User
+	/**
+	 * @api {post} /user Request create user
+	 * @apiName Create
+	 * @apiGroup User
 	 * @apiVersion 1.0.0
-     *
-     * @apiSuccess {Number} Number of total users.
-     */
-    UserRouter.get('/user/count', API.count);
+	 *
+	 * @apiParam {String} name Name of the user
+	 * @apiParam {String} email Email of the user
+	 *
+	 */
+	UserRouter.post('/user', API.create);
 
-    /**
-     * @api {get} /user/:slug Request User information
-     * @apiName GetUser
-     * @apiGroup User
+	/**
+	 * @api {get} /user/count Request count users
+	 * @apiName Count
+	 * @apiGroup User
 	 * @apiVersion 1.0.0
-     *
-     * @apiParam {String} slug user unique.
-     *
-     * @apiSuccess {String} name Name of the User.
-     * @apiSuccess {String} email  Email of the User.
-     * @apiSuccess {String} slug  Slug of the User.
-     * @apiSuccess {String} createdAt  Created date of the User.
-     */
-    UserRouter.get('/user/:slug', API.findOne);
+	 *
+	 * @apiSuccess {Number} Number of total users.
+	 */
+	UserRouter.get('/user/count', API.count);
 
-    app.use(mount('/api/v1', UserRouter.middleware()));
+	/**
+	 * @api {get} /user/:slug Request User information
+	 * @apiName GetUser
+	 * @apiGroup User
+	 * @apiVersion 1.0.0
+	 *
+	 * @apiParam {String} slug user unique.
+	 *
+	 * @apiSuccess {String} name Name of the User.
+	 * @apiSuccess {String} email  Email of the User.
+	 * @apiSuccess {String} slug  Slug of the User.
+	 * @apiSuccess {String} createdAt  Created date of the User.
+	 */
+	UserRouter.get('/user/:slug', API.findOne);
+
+
+	app.use(mount('/api/v1', UserRouter.middleware()));
 };
